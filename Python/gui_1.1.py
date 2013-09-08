675a6ce2f0c29178327d0151ed59ad46c67e3467
Eric Brunner <eric.mhr.brunner@gmail.com>
2013-09-08 12:13:01 +0200

    ajout de commentaitres (anglais) à gui_1.1.py

--- Python/gui_1.1.py
+++ Python/gui_1.1.py
@@ -43,25 +43,34 @@ class UgvGui:
 
 
 #Variables are stored in class attributes
+    
     speed = 0
-    rotation = 90
-   
+    rotation = 0
+    rot = "c"
+    spd = "s"
+
+
     def __init__(self, master):
 
 
         #Initiate grafical interface
         self.master = master
+
         #Make and place buttons, labels and text zones
+        #Speed Label
         self.speed_label = Label(master, text="Speed %i " % (self.speed))
         self.speed_label.grid(column= 1, sticky= S)
-
+        
+        #Rotation Label
         self.rotation_label = Label(master, text="Rotation %i "
                                     % (self.rotation))
         self.rotation_label.grid(column= 1)
 
+        #Quit button
         self.exit = Button(master, text= "QUIT", fg="red", command=master.quit)
         self.exit.grid(column= 1, sticky= SE)
 
+        #Message box
         self.messages = Text(master, height= 20, width= 50, 
                              background= "white", border= 2)
         self.messages.grid(column= 0, row= 0, rowspan= 3)
@@ -71,10 +80,10 @@ class UgvGui:
         
         #Bind key events to class functions
         master.bind("<Escape>", self.escape)
-        master.bind("<Shift-Up>", self.shift_up)
-        master.bind("<Shift-Down>", self.shift_down)
-        master.bind("<Shift-Right>", self.shift_right)
-        master.bind("<Shift-Left>", self.shift_left)
+        master.bind("<w>", self.faster)
+        master.bind("<s>", self.slower)
+        master.bind("<d>", self.turnMore)
+        master.bind("<a>", self.turnLess)
 	master.bind('<KeyPress-Up>', self.upPress)
 	master.bind('<KeyPress-Down>', self.downPress)
 	master.bind('<KeyPress-Right>', self.rightPress)
@@ -84,82 +93,100 @@ class UgvGui:
 	master.bind('<KeyRelease-Right>', self.rightRelease)
 	master.bind('<KeyRelease-Left>', self.leftRelease)
 	
+
     #Define on key-stroke-event functions
+
+    #"<Escape> quits application"
     def escape(self, event):
         print "Quit: UgvGui"
         #Quits the application
         self.master.destroy()
 
-    def shift_up(self, event):
+    #Set speed and rotation then send to arduino
+    def faster(self, event):
         if self.speed < 200:
             #set new speed if in range
             self.speed = self.speed + 20
             self.speed_label.config(text = "Speed %i" % (self.speed))
             print "SPEED: new speed is %i" % (self.speed)
+            ser.write(self.spd+ ",%i" % (self.speed)+ "|")
         else:
             #if the the new speed should be out of range warn the user
             print "SPEED: warning! max speed is reached"
             self.messages.insert(END, "SPEED: warning! max speed is reached \n")
 
-    def shift_down(self, event):
+    def slower(self, event):
         if self.speed > 0:
             self.speed = self.speed - 20
             self.speed_label.config(text = "Speed %i" % (self.speed))
             print "SPEED: new speed is %i" % (self.speed)
+            ser.write(self.spd+ ",%i" % (self.speed)+ "|")
         else:
             print "SPEED: warning! minimal speed is reached"
             self.messages.insert(END, "SPEED: warning! minimal speed is reached \n") 
-        
-    def shift_right(self, event):
-        if self.rotation < 140:
+
+    def turnMore(self,event):
+        if self.rotation < 50:
             self.rotation = self.rotation + 10
             self.rotation_label.config(text = "Rotation %i" % (self.rotation))
             print "ROTATION: new angle is %i" % (self.rotation)
+            ser.write(self.rot+ ",%i" % (self.rotation)+ "|")
         else:
             print "ROTATION: warning! max angle is reached"
             self.messages.insert(INSERT, "ROTATION: warning! max angle is reached \n") 
-    
-    def shift_left(self, event):
-        if self.rotation > 50:
+
+    def turnLess(self, event):
+        if self.rotation > 0:
             self.rotation = self.rotation - 10
             self.rotation_label.config(text = "Rotation %i" % (self.rotation))
             print "ROTATION: new angle is %i" % (self.rotation)
+            ser.write(self.rot+ ",%i" % (self.rotation)+ "|")
         else:
             print "ROTATION: warning! minimal angle is reached"
-            self.messages.insert(INSERT, "ROTATION: warning! minimal angle is reached \n") 
+            self.messages.insert(INSERT, "ROTATION: warning! minimal angle is reached \n")
 
-    
+    #Send message to Arduino when key pressed or released
+    #with user-set speed and rotation
+    #Allows pilot to accelerate the drone while moving
     def upPress(self, event):
+        self.spd = "f"
 	print 'f: %i' % (self.speed)
-	ser.write('f')
+	ser.write("f,"+ str(self.speed)+ "|") 
 
     def downPress(self, event):
+        self.spd = "b"
 	print 'b: %i' % (self.speed)
-	ser.write('b')
+	ser.write("b,"+ str(self.speed)+ "|")
 
     def rightPress(self, event):
+        self.rot = "r"
 	print 'r: %i' % (self.rotation)
-	ser.write('r')
+	ser.write('r,'+ str(self.rotation)+ "|")
 
     def leftPress(self, event):
+        self.rot = "l"
 	print 'l: %i' % (self.rotation) 
-	ser.write('l')
+	ser.write('l,'+ str(self.rotation)+ "|")
     
     def upRelease(self, event):
+        self.spd = "s"
 	print 'stop'
-	ser.write('s')
+	ser.write('s|')
 
     def downRelease(self, event):
+        self.spd = "s"
 	print 'stop'
-	ser.write('s')
+	ser.write('s|')
 
     def rightRelease(self, event):
+        self.rot = "c"
 	print 'center'
-	ser.write('c')
+	ser.write('c|')
 
     def leftRelease(self, event):
+        self.rot = "c"
 	print 'center'
-	ser.write('c')             
+	ser.write('c|')             
 
 
 #set auto repeat mode to off
@@ -186,5 +213,3 @@ root.after(500, update_messages)
 
 #initiate gui mainloop
 root.mainloop()
-
-
