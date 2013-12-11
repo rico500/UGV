b8160d19289cb432497d574797937b6ffd24bfb6
rico500 <eric.mhr.brunner@gmail.com>
2013-12-11 21:46:50 +0100

    Update ToArduino2.ino

--- Sketchbook/ToArduino2/ToArduino2.ino
+++ Sketchbook/ToArduino2/ToArduino2.ino
@@ -1,28 +1,10 @@
 #include<stdlib.h>
 #include <Servo.h>
-#include <NewPing.h>
 
 //Define pins for motor
 #define A 12
 #define B 11
 #define E 10
-
-//Define pins for Sensors
-#define TRIGGER_PIN1  7  
-#define ECHO_PIN1     8
-#define TRIGGER_PIN2  2
-#define ECHO_PIN2     3
-#define TRIGGER_PIN3  4  
-#define ECHO_PIN3     5
-#define MAX_DISTANCE 200 
-
-NewPing sonar1(TRIGGER_PIN1, ECHO_PIN1, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
-NewPing sonar2(TRIGGER_PIN2, ECHO_PIN2, MAX_DISTANCE);
-NewPing sonar3(TRIGGER_PIN3, ECHO_PIN3, MAX_DISTANCE);
-
-unsigned int pingSpeed = 200; // How frequently are we going to send out a ping (in milliseconds). 50ms would be 20 times a second.
-unsigned long pingTimer;     // Holds the next ping time.
-unsigned int cm1, cm2, cm3; //save distances
 
 char dir;
 int intspd;
@@ -39,28 +21,10 @@ void setup(){
   pinMode(E, OUTPUT);
   
   steer.attach(6);
-  
-  pingTimer = millis(); // Start now.
 }
 
 
 void loop(){
-  
-  //if it's time to send a ping...
-  if (millis() >= pingTimer) {   // pingSpeed milliseconds since last ping, do another ping.
-    pingTimer += pingSpeed;      // Set the next ping time.
-    sonar1.ping_timer(echoCheck1); // Send out the ping
-    sonar2.ping_timer(echoCheck2);
-    sonar3.ping_timer(echoCheck3);
-    
-    //send distances to raspberry
-    Serial.print("s|");
-    Serial.print(cm1);
-    Serial.print("|");
-    Serial.print(cm2);
-    Serial.print("|");
-    Serial.print(cm3);
-  }
   
   //if there is something to read...
   if(Serial.available()){
@@ -154,23 +118,5 @@ void halt(){
   digitalWrite(A, LOW);
   digitalWrite(B, LOW);
   digitalWrite(E, LOW);
-}
-
-void echoCheck1() { 
-  if (sonar1.check_timer()) { 
-    cm1 = sonar1.ping_result / US_ROUNDTRIP_CM;   
-  }
-}
-
-void echoCheck2() { 
-  if (sonar2.check_timer()) { 
-    cm2 = sonar2.ping_result / US_ROUNDTRIP_CM;   
-  }
-}
-
-void echoCheck3() { 
-  if (sonar3.check_timer()) { 
-    cm3 = sonar3.ping_result / US_ROUNDTRIP_CM;   
-  }
 }
 
